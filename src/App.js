756076b7291a492d8f8631016929ffebb5df48f5
Arya Prianda <aryaprianda4813@gmail.com>
2022-06-03 01:16:34 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import Beranda from "./components/beranda";
+import Navbar from "./components/navbar";
+import ManajemenBuku from "./components/ManajemenBuku";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Beranda />} />
+          <Route path="/manajemen-buku" element={<ManajemenBuku />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
